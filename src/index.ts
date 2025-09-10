0f039a93748d38a7d861527510040a139c8cf2e8
denisgrushkin <denis.grushkin@maddevs.io>
2025-09-10 17:55:54 +0600

    Add shopArtists GraphQL extension to retrieve artists by shop documentId; update index to register the new extension.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { citiesExtension } from './extensions/graphql/cities';
+import { shopArtistsExtension } from './extensions/graphql/shop-artists';
 import { usersPermissionsExtension } from './extensions/users-permissions';
 
 export default {
   register({ strapi }) {
     strapi.plugin('graphql').service('extension').use(citiesExtension);
+    strapi.plugin('graphql').service('extension').use(shopArtistsExtension);
     strapi.plugin('graphql').service('extension').use(usersPermissionsExtension);
   },
 };
