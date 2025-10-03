db7532ccea62c28a04b01a15f3807241120c536b
denisgrushkin <denis.grushkin@maddevs.io>
2025-10-03 17:56:19 +0600

    Refactor filter-blocked policy to support both GraphQL and REST API. Implemented conditional logic for applying blocked filters based on the request type.

--- src/policies/filter-blocked.ts
+++ src/policies/filter-blocked.ts
@@ -1,13 +1,28 @@
 export default async (policyContext) => {
-  const { args } = policyContext;
+  const { args, request } = policyContext;
 
-  args.filters = {
-    ...args.filters,
-    or: [
-      { blocked: { null: true } },
-      { blocked: { eq: false } },
-    ],
-  };
+  // For GraphQL
+  if (args) {
+    const blockedFilter = {
+      or: [
+        { blocked: { null: true } },
+        { blocked: { eq: false } },
+      ],
+    };
+    args.filters = {
+      and: [args?.filters, blockedFilter],
+    };
+  } else {
+    // For REST API
+    const blockedFilter = { $or: [{ blocked: { $ne: true } }, { blocked: { $null: true } }] };
+    const q = request?.query ?? {};
+    if (q.filters) {
+      q.filters = { $and: [q.filters, blockedFilter] };
+    } else {
+      q.filters = blockedFilter;
+    }
+    request.query = q;
+  }
 
   return true;
 };
