c8d3f7195c505c43877242dd3a3dfc45c7e246e1
denisgrushkin <denis.grushkin@maddevs.io>
2025-10-06 18:27:37 +0600

    Refactor lifecycle hooks in shop-request to handle user creation and email notifications based on draft status

--- src/api/shop-request/content-types/shop-request/lifecycles.ts
+++ src/api/shop-request/content-types/shop-request/lifecycles.ts
@@ -3,18 +3,39 @@
  * Automatically handle user assignment without custom controllers
  */
 
+import { UserType } from '../../../../interfaces/enums';
+
 export default {
   // Before updating, prevent changing the user (except from admin panel)
-  async afterCreate(event) {
+  async beforeCreate(event) {
     const { data } = event.params;
 
-    if (data?.documentId) {
-      await strapi.documents('api::shop-request.shop-request').update({
-        documentId: data.documentId,
-        data: {
-          tempPassword: Math.random().toString(36).substring(2, 7),
-        },
-      });
+    const isDraft = !data.publishedAt;
+
+    if (isDraft) {
+      data.tempPassword = Math.random().toString(36).substring(2, 8);
+    } else {
+      try {
+        await strapi.documents('plugin::users-permissions.user').create({
+          data: {
+            username: data.name,
+            email: data.email,
+            password: data.tempPassword,
+            type: UserType.SHOP,
+          },
+          status: 'published',
+        });
+        await strapi.documents('api::shop-request.shop-request').delete({
+          documentId: data.documentId,
+        });
+        await strapi.plugins.email.services.email.send({
+          to: data.email,
+          subject: 'Hello from Strapi!',
+          text: 'This is a test email from Strapi v5.',
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
   },
 };
