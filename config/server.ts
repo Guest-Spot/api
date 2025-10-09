46deaf95ac1f2f4eaee733430a4b975c5d22a07b
denisgrushkin <denis.grushkin@maddevs.io>
2025-10-09 18:26:30 +0600

    Update server configuration to include default APP_KEYS for enhanced security and functionality.

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', '8RX093hj2JS/wMuWLglK0Q==,uHrjVTi6tNmk/E+MwSkIxw==,bX9SWSiinrXzcbb6vpy6mw==,hujLkyAHTym+O8IUg5RNUA=='),
   },
   url: env('PUBLIC_URL', 'http://localhost:1337'),
 });
