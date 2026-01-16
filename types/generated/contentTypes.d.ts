e4b4bceded94fedbf832d08eea8014a6f828ca31
denisgrushkin <denis.grushkin@maddevs.io>
2026-01-16 14:32:02 +0600

    Update profile schema to change latitude and longitude types from decimal to float
    - Modified the profile schema to use 'float' for 'lat' and 'lng' fields instead of 'decimal' for improved precision in geographical data representation.
    - Updated type definitions to reflect these changes in the generated TypeScript files.

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -771,9 +771,9 @@ export interface ApiProfileProfile extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    lat: Schema.Attribute.Decimal;
+    lat: Schema.Attribute.Float;
     links: Schema.Attribute.Component<'contact.social-links', true>;
-    lng: Schema.Attribute.Decimal;
+    lng: Schema.Attribute.Float;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
